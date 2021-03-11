55d70ebfb837db70359cae0bd044f6b41dcc971f
aviadamd <aviadamd@gmail.com>
2021-03-11 23:17:46 +0200

    fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,11 +4,8 @@
 # BROWSER
 # MODULE
 
-echo "Checking if hub is ready - $HUB_HOST"
-
 while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
-do
-  sleep 1
+do sleep 1
 done
 
 # start the java command
