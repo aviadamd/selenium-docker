f396c065b388c6521955d37ad72176d72842da85
aviadamd <aviadamd@gmail.com>
2021-03-13 01:03:24 +0200

    fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,7 +6,7 @@
 
 echo "Checking if hub is ready - $HUB_HOST"
 
-while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status )" != "true" ]
 do
 	sleep 1
 done
